b6bc5bba20c907de2d0f41c8f2584433ace69bb8
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2024-02-05 20:31:22 +0700

    feat(prisma): add Prisma ORM for database management

---
+++ prisma/migrations/20240204081454_initalization/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `Product` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `price` INTEGER NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `category` VARCHAR(191) NOT NULL,
+    `image` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Product_title_key`(`title`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;

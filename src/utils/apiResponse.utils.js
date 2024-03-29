348dba838340a5b58926bdd2dff47632710ee3d9
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2024-03-29 18:05:50 +0700

    feat: add utils api response

---
+++ src/utils/apiResponse.utils.js
@@ -0,0 +1,65 @@
+const { StatusCodes } = require("http-status-codes");
+
+const errorCustomMessage = (errors) =>
+  errors.details.reduce(
+    (acc, curr) => ({
+      ...acc,
+      [curr.path]: curr.message,
+    }),
+    {}
+  );
+
+module.exports = {
+  apiResponse: (code, status, message, data) => {
+    const result = {};
+    result.code = code || StatusCodes.OK;
+    result.status = status || "OK";
+    result.message = message;
+    result.data = data;
+
+    return result;
+  },
+  apiResponseValidationError: (errors) => {
+    const result = {};
+    result.code = StatusCodes.UNPROCESSABLE_ENTITY;
+    result.status = "UNPROCESSABLE_ENTITTY";
+    result.message = "The given data was invalid";
+    result.errors = errorCustomMessage(errors);
+
+    return result;
+  },
+  notFoundResponse: (message) => {
+    const result = {};
+    result.code = StatusCodes.NOT_FOUND;
+    result.code = "NOT_FOUND";
+    result.message = `${
+      message ? `${message} Not Found` : "Resource Not Found"
+    }`;
+
+    return result;
+  },
+  unAuthorizedResponse: (message) => {
+    const result = {};
+    result.code = StatusCodes.UNAUTHORIZED;
+    result.status = "UNAUTHORIZED";
+    result.message = `Unauthorized.${message ? `${message}` : ""}`;
+
+    return result;
+  },
+  noContentResponse: (message) => {
+    const result = {};
+    result.code = StatusCodes.NO_CONTENT;
+    result.status = "NO_CONTENT";
+    result.message = message;
+
+    return result;
+  },
+  badRequestResponse: (message) => {
+    const result = {};
+    result.code = StatusCodes.BAD_REQUEST;
+    result.status = "BAD_REQUEST";
+    result.message = message;
+
+    return result;
+  },
+};

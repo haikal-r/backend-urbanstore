a701834e1e3278f38aa269afc64e18dcb7dd215c
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2024-03-29 17:59:41 +0700

    feat: add libs and create routes

--- src/index.js
+++ src/index.js
@@ -1,20 +1,42 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const { routes } = require("./routes");
+const { StatusCodes: status } = require("http-status-codes")
+const { apiResponse } = require('./utils/apiResponse.utils')
+const cookieParser = require("cookie-parser");
+const cors = require("cors")
 
 dotenv.config();
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors())
+app.use(cookieParser())
 
-app.get("/api", (req, res) => {
-  res.send("Selamat datang di api akh anjaay");
-});
 
-const productController = require("./product/product.controller")
+app.get('/', (req, res) => {
+  res.status(status.OK).json(
+    apiResponse(status.OK, 'OK', 'Welcome to the initial Express API Structure')
+  )
+})
 
-app.use("/products", productController)
+app.get((req, res) => {
+  res.status(status.NOT_FOUND).json(
+    apiResponse(status.NOT_FOUND, 'NOT_FOUND', 'The requested resource could not be found.')
+  )
+})
 
-const PORT = process.env.PORT;
+app.get((err, req, res, next) => {
+  res.status(status.INTERNAL_SERVER_ERROR).json(
+    apiResponse(status.INTERNAL_SERVER_ERROR, 'INTERNAL_SERVER_ERROR', err.message)
+  )
+})
+
+// Routes
+routes(app);
+
+// Port
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("Express API running in port: " + PORT);
+  console.info(`Server is running on port ${PORT}, http://localhost:${PORT}` );
 });

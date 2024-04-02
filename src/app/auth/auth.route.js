cff1ef8ec9de6cb27b58d9f86c42ced82ee1974d
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2024-04-02 15:26:15 +0700

    feat: update auth route

--- src/app/auth/auth.route.js
+++ src/app/auth/auth.route.js
@@ -7,7 +7,14 @@ const {
   RefreshToken,
   Me,
   Logout,
+  SendEmail,
+  ForgotPassword,
 } = require("./auth.controller");
+const {
+  authentication,
+  refreshToken,
+} = require("../../middlewares/authentication.middleware");
+const { RegisterMiddleware, LoginMiddleware, SendEmailMiddleware, ForgotPasswordMiddleware } = require("./auth.middleware");
 
 const router = express.Router();
 
@@ -16,10 +23,14 @@ router.get("/auth/google", LoginByGoogle);
 router.get("/auth/google/callback", LoginGoogleCallback);
 
 // Login Reguler
-router.post("/register", Register);
-router.post("/login", Login);
-router.get("/refresh-token", RefreshToken);
-router.post("/logout", Logout);
-router.post("/me", Me);
+router.post("/register", RegisterMiddleware, Register);
+router.post("/login", LoginMiddleware, Login);
+router.get("/me", authentication, Me);
+router.post("/logout", authentication, Logout);
+
+router.post("/send-email",SendEmailMiddleware, SendEmail)
+router.post("/forgot-password/:token", ForgotPasswordMiddleware, ForgotPassword)
+
+router.get("/refresh-token", refreshToken, RefreshToken);
 
 module.exports = router;

63d4e1851bd4225e62f9b1f8140c55c69582211c
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2024-04-02 15:26:48 +0700

    feat: add authentication user middleware

---
+++ src/middlewares/authentication.middleware.js
@@ -0,0 +1,43 @@
+const { unAuthorizedResponse, noContentResponse } = require("../utils/apiResponse.utils");
+const { verifyAccessToken, verifyRefreshToken } = require("../utils/jwt.utils");
+
+const tokenErr = (e) => {
+  if (e.name === "TokenExpiredError") {
+    return unAuthorizedResponse("Token Expired. Please login again.");
+  }
+  if (e.name === "JsonWebTokenError") {
+    return unAuthorizedResponse("Invalid Token.");
+  }
+};
+
+module.exports = {
+  authentication: (req, res, next) => {
+    try {
+      const bearer = req.headers.authorization;
+      if (!bearer) throw unAuthorizedResponse();
+
+      const token = bearer.split(" ")[1];
+      req.user = verifyAccessToken(token);
+
+      next();
+    } catch (e) {
+      const err = tokenErr(e);
+      if (err) return res.status(err.code).json(err);
+      return res.status(e.code).json(e);
+    }
+  },
+  refreshToken: (req, res, next) => {
+    try {
+      const { refreshToken } = req.cookies;
+      if (!refreshToken) throw noContentResponse("No Refresh Token Provided");
+
+      req.refreshToken = verifyRefreshToken(refreshToken);
+
+      next();
+    } catch (e) {
+      const err = tokenErr(e);
+      if (err) return res.status(err.code).json(err);
+      return res.status(e.code).json(e);
+    }
+  },
+};

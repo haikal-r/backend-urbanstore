3e2bf4b6d37292e9d6e3f145cfe8cd219e8363e9
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2024-08-28 14:57:06 +0700

    feat: update config

--- src/config/midtrans.config.js
+++ src/config/midtrans.config.js
@@ -2,12 +2,12 @@ const midtrans = require('midtrans-client')
 
 module.exports = {
   CoreAPI: new midtrans.CoreApi({
-    isProduction: process.env.MIDTRANS_IS_PRODUCTION === "false",
+    isProduction: process.env.MIDTRANS_IS_PRODUCTION === "true",
     serverKey: process.env.MIDTRANS_SERVER_KEY,
     clientKey: process.env.MIDTRANS_CLIENT_KEY,
   }),
   Snap: new midtrans.Snap({
-    isProduction: false,
+    isProduction: process.env.MIDTRANS_IS_PRODUCTION === 'true',
     serverKey: process.env.MIDTRANS_SERVER_KEY,
     clientKey: process.env.MIDTRANS_CLIENT_KEY,
   })
